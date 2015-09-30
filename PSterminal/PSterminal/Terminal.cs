1619d4664d00d2f008533913087b867d7289962b
seigneurWood <lis.vlad96@gmail.com>
2015-09-30 23:52:38 +0300

    test

---
+++ PSterminal/PSterminal/Terminal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace PSterminal
+{
+    public class Terminal
+    {
+        private string _script;
+
+        public string Script
+        {
+            get { return _script; }
+            set { _script = value; }
+        }
+
+
+
+    }
+}
---
+++ PSterminal/PSterminal/UndoRedoCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSterminal
+{
+    public class UndoRedoCommand
+    {
+        private Stack<Command> _Undocommands = new Stack<Command>();
+        private Stack<Command> _Redocommands = new Stack<Command>();
+
+        public void Redo(int levels)
+        {
+            for (int i = 1; i <= levels; i++)
+            {
+                if (_Redocommands.Count != 0)
+                {
+                    Command command = _Redocommands.Pop();
+                    command.Execute();
+                    _Undocommands.Push(command);
+                }
+
+            }
+        }
+
+        public void Undo(int levels)
+        {
+            for (int i = 1; i <= levels; i++)
+            {
+                if (_Undocommands.Count != 0)
+                {
+                    Command command = _Undocommands.Pop();
+                    command.UnExecute();
+                    _Redocommands.Push(command);
+                }
+
+            }
+        }
+    }
+}
